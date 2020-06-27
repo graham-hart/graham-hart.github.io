2606084acb4841871955ce76c460799ba2d33448
graham-hart <graham@harts.net>
2020-06-27 12:13:38 -0700

    idk bruh

--- Games/Projects/Snake/src/sketch.js
+++ Games/Projects/Snake/src/sketch.js
@@ -7,7 +7,6 @@ let snakeStartLength = 4;
 let snake;
 let food;
 /*GRAPHICS*/
-let fonts = [];
 let font;
 let snakeColor = "#40fa1dff";
 let foodColor = "#fa401dff";
@@ -19,9 +18,7 @@ let startFR = fr;
 /*Misc. vars*/
 let paused = true;
 function preload() {
-  //Add more fonts here
-  fonts.push(loadFont("assets/fonts/fffforward.TTF"));
-  font = fonts[0];
+  font = loadFont("assets/fonts/fffforward.TTF");
 }
 function setup() {
   createCanvas(size, size);
@@ -40,56 +37,6 @@ function draw() {
   food.show();
   showScore();
 }
-function keyPressed() {
-  if (keyCode === UP_ARROW && snake) {
-    if (snake.velocity.y === 0) {
-      snake.velocity = createVector(0, -1);
-    }
-  } else if (keyCode === DOWN_ARROW) {
-    if (snake.velocity.y === 0) {
-      snake.velocity = createVector(0, 1);
-    }
-  } else if (keyCode === LEFT_ARROW) {
-    if (snake.velocity.x === 0) {
-      snake.velocity = createVector(-1, 0);
-    }
-  } else if (keyCode === RIGHT_ARROW) {
-    if (snake.velocity.x === 0) {
-      snake.velocity = createVector(1, 0);
-    }
-  } else if (key === "g") {
-    if (graphicsType === "lineArt") {
-      graphicsType = "filled";
-    } else {
-      graphicsType = "lineArt";
-    }
-  } else if (keyCode === ENTER) {
-    if (paused) {
-      loop();
-      paused = false;
-    } else {
-      paused = true;
-      noLoop();
-    }
-  }
-}
-function mousePressed() {
-  if (paused) {
-    paused = false;
-    loop();
-  } else {
-    paused = true;
-    noLoop();
-  }
-}
-function showScore() {
-  push();
-  textFont(font);
-  textSize(32);
-  fill(255);
-  text(snake.segments.length - snakeStartLength, width / 2, 70);
-  pop();
-}
 function restart() {
   diedText();
   paused = true;
@@ -97,13 +44,4 @@ function restart() {
   food = new Food();
   snake = new Snake(boxCount / 2, boxCount / 2);
   fr = startFR;
-}
-function diedText() {
-  push();
-  textAlign(CENTER);
-  fill(255);
-  textSize(32);
-  textFont(font);
-  text("YOU DIED! PRESS ENTER TO RESTART!", width / 2, height / 2);
-  pop();
 }
